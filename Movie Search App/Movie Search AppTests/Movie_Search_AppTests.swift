0d53bf72df3591bdd0f4d21a98de07d2c139b0f5
Kaique <kaique.lopes3271@gmail.com>
2021-11-06 21:30:27 -0300

    First Commit

---
+++ Movie Search App/Movie Search AppTests/Movie_Search_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Movie_Search_AppTests.swift
+//  Movie Search AppTests
+//
+//  Created by Kaique Lopes de Oliveira on 06/11/21.
+//
+
+import XCTest
+@testable import Movie_Search_App
+
+class Movie_Search_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
